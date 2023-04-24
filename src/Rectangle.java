2177f49e982a635fa6ca6e3caddb64a961617b04
modengann <99417989+modengann@users.noreply.github.com>
2023-04-24 10:47:47 -0400

    init

---
+++ src/Rectangle.java
@@ -0,0 +1,18 @@
+/** INSTRUCTIONS:
+ * 
+ * 1. Define a class called "Rectangle" with the following attributes:
+ *     -width (double)
+ *     -height (double)
+ * 
+ * 2. Create a constructor that takes in the width and height and initializes the class.
+ * 
+ * 3.  Write a method called "getArea" that calculates and returns the area of the rectangle.
+ * 
+ * 4. Write a method called "getPerimeter" that calculates and returns the perimeter of the rectangle.
+ * 
+ */
+
+
+public class Rectangle {
+    
+}
